f27a5a99df096342b3f7ee180335ae5654300ff0
roigcarlo <roigcarlo@gmail.com>
2015-04-05 15:54:23 +0200

    Added bfecc kernel function. It probably does not even compile yet...

--- include/stencils.h
+++ include/stencils.h
@@ -15,3 +15,44 @@ inline void stencilCross(T * gridA, T * gridB,
     gridA[cell - (Y+BW)*(X+BW)] +             // Front
     gridA[cell + (Y+BW)*(X+BW)] ) * ONESIX;   // Back
 }
+
+/**
+ * Performs the bfecc operation over a given element
+ * @phi:        Result of the operation
+ * @phi_auxA:   first  operator
+ * @phi_auxB:   second operator
+ * @field:      field
+ * @dx:         diferential of space
+ * @dt:         diferential of time
+ * sign:        direction of the interpolation ( -1.0 backward, 1.0 forward )
+ * weightA:     weigth of the first operator (A)
+ * weightB:     weigth of the second operator (B)
+ **/ 
+template <
+    typename VariableType, 
+    typename Variable3DType,
+    typename IndexFunction,
+    typename InterpolateFunction
+>
+void bfecc(VariableType * phi, VariableType * phi_auxA, VariableType * phi_auxB, 
+    Variable3DType * field,
+    const double &dx,   const double &dt,
+    const double &sign, const double &weightA, const double &weightB,
+    const uint &i, const uint &j, const uint &k,
+    const uint &X, const uint &Y, const uint &Z) {
+
+  uint cell = IndexFunction(i,j,k,X,Y,Z);
+
+  Triple origin;
+  Triple displacement;
+
+  origin[0] = i * dx;
+  origin[1] = j * dx;
+  origin[2] = k * dx;
+ 
+  for(int d = 0; d < 3; d++) {
+    displacement[d] = origin[d] + sign * field[cell][d]*dt;
+  }
+
+  phi[cell] = weightA * phi_auxA[cell] + weightB * InterpolateFunction(displacement,phi_auxB,X,Y,Z);
+}
