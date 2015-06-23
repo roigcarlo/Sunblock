9643eedca3fafdd36e806381f289f3e27f74fdd5
Carlos Roig <roigcarlo@gmail.com>
2015-06-23 09:23:21 +0200

    Removed old useless code

--- include/solver_bfecc.h
+++ include/solver_bfecc.h
@@ -139,18 +139,3 @@ public:
     Phi[cell] = WeightA * PhiAuxA[cell] + WeightB * iPhi;
   }
 };
-
-// This does not belong here! put it in a class
-template <typename T>
-inline void stencilCross(T * gridA, T * gridB,
-    const uint &cell, 
-    const uint &X, const uint &Y, const uint &Z) {
-  
-  gridB[cell] = (
-    gridA[cell - 1]   +                       // Left
-    gridA[cell + 1]   +                       // Right
-    gridA[cell - (X+BW)]   +                  // Up
-    gridA[cell + (X+BW)]   +                  // Down
-    gridA[cell - (Y+BW)*(X+BW)] +             // Front
-    gridA[cell + (Y+BW)*(X+BW)] ) * ONESIX;   // Back
-}
